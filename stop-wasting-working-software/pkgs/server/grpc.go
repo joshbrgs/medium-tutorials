38202693478ac70c3d810477f4cbddc587d39841
Josh Burgess <burgessj247@gmail.com>
2024-09-04 13:42:03 -0400

    Adds proto files and fixes server pkg for grpc

--- stop-wasting-working-software/pkgs/server/grpc.go
+++ stop-wasting-working-software/pkgs/server/grpc.go
@@ -4,41 +4,27 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 // GRPCServerOption represents a function that modifies gRPC server options
-type GRPCServerOption func(*grpc.Server)
-
-// WithGRPCAddress sets the address option for the gRPC server
-func WithGRPCAddress(address string) GRPCServerOption {
-	return func(s *grpc.Server) {
-		s.Addr = address
-	}
-}
+type GRPCServerOption func(*grpc.ServerOption)
 
 // WithGRPCMaxConcurrentStreams sets the MaxConcurrentStreams option for the gRPC server
-func WithGRPCMaxConcurrentStreams(maxConcurrentStreams uint32) GRPCServerOption {
-	return func(s *grpc.Server) {
-		s.MaxConcurrentStreams = maxConcurrentStreams
-	}
+func WithGRPCMaxConcurrentStreams(maxConcurrentStreams uint32) grpc.ServerOption {
+	return grpc.MaxConcurrentStreams(maxConcurrentStreams)
 }
 
 // WithGRPCIdleTimeout sets the IdleTimeout option for the gRPC server
-func WithGRPCIdleTimeout(idleTimeout time.Duration) GRPCServerOption {
-	return func(s *grpc.Server) {
-		s.IdleTimeout = idleTimeout
-	}
+func WithGRPCIdleTimeout(idleTimeout time.Duration) grpc.ServerOption {
+	return grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle: idleTimeout,
+	})
 }
 
 // NewGRPCServer initializes a new instance of gRPC server with the provided options
-func NewGRPCServer(opts ...GRPCServerOption) *grpc.Server {
-	// Create a new gRPC server instance
-	s := grpc.NewServer()
-
-	// Apply provided options
-	for _, opt := range opts {
-		opt(s)
-	}
-
-	return s
+func NewGRPCServer(opts ...grpc.ServerOption) *grpc.Server {
+	// Create a new gRPC server instance with the provided options
+	return grpc.NewServer(opts...)
 }
+
